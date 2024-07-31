a791ef28eba91588f524f5e428086e75827b790a
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-07-31 13:18:32 +0900

    Finished Chapter 4.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ by `freeCodeCamp.org`, `Per Harald Borgen`
 1. Intro
 2. Passenger counter app
 3. Setting up file
-4.
+4. Create variable
 5.
 6.
 7.
--- src/people-counter/index.js
+++ src/people-counter/index.js
@@ -1,1 +1,11 @@
-document.getElementById("count-el").innerText = 5;
+//document.getElementById("count-el").innerText = 5;
+
+let count = 0;
+
+console.log(count);
+
+// 1. Create a variable, myAge, and set its valeu toyour age.
+myAge = 55;
+
+// 2. Log the myAge variable to the console.
+console.log(myAge);
